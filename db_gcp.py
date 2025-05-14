438d72b90a0b1a028959bee5dc559798808d8fdc
rayl <dwadeflashback@gmail.com>
2025-05-13 23:41:40 -0400

    use config.py

--- db_gcp.py
+++ db_gcp.py
@@ -1,16 +1,7 @@
 # test connect to gcp
 import pyodbc
 
-conn = pyodbc.connect(
-    'DRIVER={ODBC Driver 17 for SQL Server};'
-    'SERVER=34.58.90.139,1433;'
-    'DATABASE=cardb1;'
-    'UID=user2;'
-    'PWD=12345678;'
-    'Encrypt=yes;'
-    'TrustServerCertificate=yes;'
-    'Connection Timeout=30;'
-)
+from config import conn
 
 cursor = conn.cursor()
 
