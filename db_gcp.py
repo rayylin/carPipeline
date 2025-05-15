9af7bc12096dad7177074502a13e23bb8bb48b65
rayl <dwadeflashback@gmail.com>
2025-05-14 23:31:16 -0400

    tmp

--- db_gcp.py
+++ db_gcp.py
@@ -5,20 +5,20 @@ from config import conn
 
 cursor = conn.cursor()
 
-cursor.execute("""CREATE TABLE stock (
-                    id NVARCHAR(20),
-                    date DATETIME,
-                    start NUMERIC(12,4),
-                    [end] NUMERIC(12,4),
-                    high NUMERIC(12,4),
-                    low NUMERIC(12,4)
-                    );""")
-cursor.execute("""
-    INSERT INTO stock (id, date, start, [end], high, low)
-    VALUES (?, ?, ?, ?, ?, ?)
-""", ('AAPL', '2025-11-05', 198.53, 198.53, 198.53, 198.53))
+# cursor.execute("""CREATE TABLE stock (
+#                     id NVARCHAR(20),
+#                     date DATETIME,
+#                     start NUMERIC(12,4),
+#                     [end] NUMERIC(12,4),
+#                     high NUMERIC(12,4),
+#                     low NUMERIC(12,4)
+#                     );""")
+# cursor.execute("""
+#     INSERT INTO stock (id, date, start, [end], high, low)
+#     VALUES (?, ?, ?, ?, ?, ?)
+# """, ('AAPL', '2025-11-05', 198.53, 198.53, 198.53, 198.53))
 
-conn.commit()
+# conn.commit()
 cursor.execute("SELECT * FROM stock")
 
 for row in cursor.fetchall():
