8191f7706559d50e19c4fb2f4467c65a5dd67403
rayl <dwadeflashback@gmail.com>
2025-05-16 23:24:59 -0400

    add new file for sql script

--- db_gcp.py
+++ db_gcp.py
@@ -2,21 +2,11 @@
 import pyodbc
 
 from config import conn
+from sqlScript import insertStock
 
 cursor = conn.cursor()
 
-# cursor.execute("""CREATE TABLE stock (
-#                     id NVARCHAR(20),
-#                     date DATETIME,
-#                     start NUMERIC(12,4),
-#                     [end] NUMERIC(12,4),
-#                     high NUMERIC(12,4),
-#                     low NUMERIC(12,4)
-#                     );""")
-# cursor.execute("""
-#     INSERT INTO stock (id, date, start, [end], high, low)
-#     VALUES (?, ?, ?, ?, ?, ?)
-# """, ('AAPL', '2025-11-05', 198.53, 198.53, 198.53, 198.53))
+cursor.execute(insertStock, ('AAPL', '2025-11-05', 198.53, 198.53, 198.53, 198.53))
 
 # conn.commit()
 cursor.execute("SELECT * FROM stock")
---
+++ sqlScript.py
@@ -0,0 +1,14 @@
+createStock = """CREATE TABLE stock (
+                    id NVARCHAR(20),
+                    date DATETIME,
+                    start NUMERIC(12,4),
+                    [end] NUMERIC(12,4),
+                    high NUMERIC(12,4),
+                    low NUMERIC(12,4)
+                    );"""
+
+insertStock = """
+    INSERT INTO stock (id, date, start, [end], high, low)
+    VALUES (?, ?, ?, ?, ?, ?)
+    """ 
+#, ('AAPL', '2025-11-05', 198.53, 198.53, 198.53, 198.53)
