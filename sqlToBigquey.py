b014ecafaf95456d78fdf0cc5a2726f99a21a9d4
rayl <dwadeflashback@gmail.com>
2025-06-05 00:41:55 -0400

    wrap as http handler

--- sqlToBigquey.py
+++ sqlToBigquey.py
@@ -1,46 +1,51 @@
-import os
-from google.cloud import bigquery
-import pandas as pd
-from config import eid, pathBigQuery, conn
-import decimal
 
-cursor = conn.cursor()
+def transfer_stock_data(request):
+    import os
+    from google.cloud import bigquery
+    import pandas as pd
+    from config import eid, pathBigQuery, conn
+    import decimal
+    import pyodbc
 
-# Set path to your JSON key file
+    cursor = conn.cursor()
 
-os.environ[eid] = pathBigQuery
+    # Set path to your JSON key file
+    os.environ[eid] = pathBigQuery
 
-# Initialize BigQuery client
-client = bigquery.Client()
-
-
-# Step 1: Get latest date from BigQuery
-query = "SELECT MAX(date) as max_date FROM `carpipe.stockPrice.stockPrice`"
-latest_date = client.query(query).result().to_dataframe().iloc[0]['max_date']
-
-print(latest_date)
+    # Initialize BigQuery client
+    client = bigquery.Client()
 
 
-# Step 2: Query SQL Server
-query_sql = "SELECT * FROM [financeDb].[dbo].[stock] WHERE [date] > ? "
-cursor.execute(query_sql, latest_date)
-rows = cursor.fetchall()
+    # Step 1: Get latest date from BigQuery
+    query = "SELECT MAX(date) as max_date FROM `carpipe.stockPrice.stockPrice`"
+    latest_date = client.query(query).result().to_dataframe().iloc[0]['max_date']
 
-# Convert to DataFrame
-columns = [column[0] for column in cursor.description]
-df_new = pd.DataFrame.from_records(rows, columns=columns)
+    print(latest_date)
 
 
-# Step 3: Upload new data to BigQuery
+    # Step 2: Query SQL Server
+    query_sql = "SELECT * FROM [financeDb].[dbo].[stock] WHERE [date] > ? "
+    cursor.execute(query_sql, latest_date)
+    rows = cursor.fetchall()
+
+    # Convert to DataFrame
+    columns = [column[0] for column in cursor.description]
+    df_new = pd.DataFrame.from_records(rows, columns=columns)
 
 
-# Sanitize DataFrame for BigQuery
-for col in df_new.columns:
-    if df_new[col].apply(lambda x: isinstance(x, decimal.Decimal)).any():
-        df_new[col] = df_new[col].astype(float)
+    # Step 3: Upload new data to BigQuery
 
-# Load to BigQuery
-client.load_table_from_dataframe(
-    df_new,
-    "carpipe.stockPrice.stockPrice"
-).result()
+
+    # Sanitize DataFrame for BigQuery
+    for col in df_new.columns:
+        if df_new[col].apply(lambda x: isinstance(x, decimal.Decimal)).any():
+            df_new[col] = df_new[col].astype(float)
+
+    # Load to BigQuery
+    client.load_table_from_dataframe(
+        df_new,
+        "carpipe.stockPrice.stockPrice"
+    ).result()
+
+
+    return "Transfer complete", 200
